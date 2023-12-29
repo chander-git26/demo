92c1ceefa0d1a81b1d2ac78c6457ac40894427df
NarraRaju <Narraraju@cosglobal.in>
2023-12-29 12:53:37 +0530

    cors error resovle

--- demo/src/main/java/com/cos/master/MasterApplication.java
+++ demo/src/main/java/com/cos/master/MasterApplication.java
@@ -7,30 +7,50 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.cos.master")
+
 public class MasterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterApplication.class, args);
 	}
 
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer(){
+//		return  new WebMvcConfigurer() {
+//			
+//			@Override
+//			public void addCorsMappings(CorsRegistry reg) {
+//				
+//				reg.addMapping("/**").allowedOrigins("*");
+//			
+//			}
+//		};
+//			
+//		
+//		
+//	}
+	
+	
 	@Bean
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config=new CorsConfiguration();
         config.setAllowCredentials(true);
         
-//        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+       config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
         config.setAllowedOrigins(Collections.singletonList("http://localhost:8280"));
 
         config.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
