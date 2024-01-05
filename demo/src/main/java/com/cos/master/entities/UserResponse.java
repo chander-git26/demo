c2e15de226cb12efc26809747cead70a527d29d0
Poornachander Akula <poornachander@cosglobal.in>
2024-01-05 12:02:21 +0530

    UserResponse class added for signIn API

---
+++ demo/src/main/java/com/cos/master/entities/UserResponse.java
@@ -0,0 +1,16 @@
+package com.cos.master.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponse {
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String userId;
+	private String mobile;
+	private String email;
+	private String password;
+}
